cdfbecb667d1b5c75baddc83c39483bc45922d03
rades12340 <rades12340@gmail.com>
2019-06-26 19:51:17 +0200

    init repos

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is scream body',
+      createdAt: '2019-06-26T12:30:20.782Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
